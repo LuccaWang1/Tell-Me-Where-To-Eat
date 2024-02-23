8ac063f55d2520ee5425933c94832557636325c2
Lucca Wang <luccawang@gmail.com>
2024-02-23 01:51:55 -0700

    Py logic and CSS

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, request
 import random
-import eatery_datasets
+from eatery_datasets import eateries
 
 app = Flask(__name__)
 
@@ -19,29 +19,30 @@ def handle_choose_one():
     eatery_type = request.form.get("kinds")
 
     if eatery_type == "":
-        eatery = random.choice(eatery_datasets.eateries[["american"]+["asian"]+["breakfast"]+["desert"]+["fast_food"]+["italian"]+["mexican"]])
+        eatery = random.choice(eateries[["american"]+["asian"]+["breakfast"]+["desert"]+["fast_food"]+["italian"]+["mexican"]])
+        return 
 
     else:
         if eatery_type == "american":
-            eatery = random.choice(eatery_datasets.american)
+            eatery = random.choice(eateries["american"])
 
         if eatery_type == "asian": 
-            eatery = random.choice(eatery_datasets.asian)
+            eatery = random.choice(eateries["asian"])
 
         if eatery_type == "breakfast": 
-            eatery = random.choice(eatery_datasets.breakfast)
+            eatery = random.choice(eateries["breakfast"])
 
         if eatery_type == "desert": 
-            eatery = random.choice(eatery_datasets.desert)
+            eatery = random.choice(eateries["desert"])
 
         if eatery_type == "fast_food": 
-            eatery = random.choice(eatery_datasets.fast_food)
+            eatery = random.choice(eateries["fast_food"])
             
         if eatery_type == "italian": 
-            eatery = random.choice(eatery_datasets.italian)
+            eatery = random.choice(eateries["italian"])
 
         if eatery_type == "mexican":
-            eatery = random.choice(eatery_datasets.mexican)
+            eatery = random.choice(eateries["mexican"])
         
     
 
