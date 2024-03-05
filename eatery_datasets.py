83b4ea068a9105eeb2f73cf445a14ccdd51518a1
Lucca Wang <luccawang@gmail.com>
2024-03-05 12:51:53 -0700

    Updated CSS with # for button id selector, reduced redundancy on font size and border radius by removing from individual selectors and putting them only under all

--- eatery_datasets.py
+++ eatery_datasets.py
@@ -21,15 +21,20 @@ breakfast = [
 
 desert = [
     "DQ",
+    "The Paleta Bar",
+    "Caliche's"
 ]
 
 fast_food = [
     "DQ", 
     "Taco Bell",
+    "Kentucky Fried Chicken",
+    "Popeye's"
 ]
 
 italian = [
     "Luna Rossa",
+    "Dion's",
 ]
 
 mexican = [
